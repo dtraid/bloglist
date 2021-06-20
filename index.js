994b36cb8e207534610fda68a8e78db597575e83
dtraid <diego@dtraid.com>
2021-06-20 11:38:46 +0200

    Exercise 4.2 Blog list, step2

--- index.js
+++ index.js
@@ -1,36 +1,10 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
+const app = require('./app'); // the actual Express application
+const http = require('http');
 const config = require('./utils/config');
-const mongoose = require('mongoose');
-const Blog = require('./models/blog');
+const logger = require('./utils/logger');
 
-const mongoUrl = config.MONGODB_URI;
+const server = http.createServer(app);
 
-mongoose.connect(mongoUrl, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true,
-});
-
-app.use(cors());
-app.use(express.json());
-
-app.get('/api/blogs', (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-app.listen(config.PORT, () => {
-  console.log(`Server running on port ${config.PORT}`);
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`);
 });
