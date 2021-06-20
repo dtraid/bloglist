5708458aea88ac9b806ee4e689f126743e9adc49
dtraid <diego@dtraid.com>
2021-06-20 11:09:17 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const config = require('./utils/config');
+const mongoose = require('mongoose');
+const Blog = require('./models/blog');
+
+const mongoUrl = config.MONGODB_URI;
+
+mongoose.connect(mongoUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false,
+  useCreateIndex: true,
+});
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/api/blogs', (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post('/api/blogs', (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
+});
